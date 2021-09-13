19cf871468bb90e0c292e144811bf44d5cf7c5b2
Nirmal <85055463+Nirmal314@users.noreply.github.com>
2021-09-13 09:08:39 +0530

    Update BST.c

--- BST.c
+++ BST.c
@@ -1,67 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "line.h"
 
-typedef struct TREE
-{
-    int data, key;
-    struct TREE *left;
-    struct TREE *right;
-} node;
+static int height = 0;
 
-void line()
+struct node
 {
-    for (int i = 0; i < 75; i++)
-    {
-        printf("-");
-    }
-    printf("\n");
-}
-node *create()
+    int data;
+    struct node *left, *right;
+};
+struct node *createNode(int key)
 {
-    node *tmp;
-    line();
-    printf("Enter the data : ");
-    tmp = (node *)malloc(sizeof(node));
-    scanf("%d", &tmp->data);
-    tmp->left = tmp->right = NULL;
+    struct node *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->data = key;
+    tmp->right = tmp->left = NULL;
     return tmp;
 }
-void insert(node *root, node *tmp)
+struct node *insertNode(struct node *root, int key)
 {
-    if (tmp->data < root->data)
-    {
-        if (root->left != NULL)
-        {
-            insert(root->left, tmp);
-        }
-        else
-        {
-            root->left = tmp;
-        }
-    }
-    if (tmp->data > root->data)
-    {
-        if (root->right != NULL)
-        {
-            insert(root->right, tmp);
-        }
-        else
-        {
-            root->right = tmp;
-        }
-    }
-}
+    if (root == NULL)
+        return createNode(key);
 
-void postorder(node *root)
-{
-    if (root != NULL)
-    {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
-    }
+    if (key < root->data)
+        root->left = insertNode(root->left, key);
+
+    else if (key > root->data)
+        root->right = insertNode(root->right, key);
+
+    return root;
 }
-void preorder(node *root)
+void preorder(struct node *root)
 {
     if (root != NULL)
     {
@@ -70,7 +39,16 @@ void preorder(node *root)
         preorder(root->right);
     }
 }
-void inorder(node *root)
+void postorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+void inorder(struct node *root)
 {
     if (root != NULL)
     {
@@ -79,43 +57,190 @@ void inorder(node *root)
         inorder(root->right);
     }
 }
+int minValue_print(struct node *root)
+{
+    while (root && root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root->data;
+}
+int maxValue_print(struct node *root)
+{
+    while (root && root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root->data;
+}
+struct node *minValue(struct node *root)
+{
+    while (root && root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+struct node *deleteNode(struct node *root, int key)
+{
+    if (root == NULL)
+        return root;
 
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+
+    else
+    {
+        if (root->left == NULL)
+        {
+            struct node *tmp = root->right;
+            free(root);
+            return tmp;
+        }
+
+        else if (root->right == NULL)
+        {
+            struct node *tmp = root->left;
+            free(root);
+            return tmp;
+        }
+
+        struct node *tmp = minValue(root->right);
+        root->data = tmp->data;
+        root->right = deleteNode(root->right, tmp->data);
+    }
+
+    return root;
+}
+int searchNode(struct node *root, int key)
+{
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+
+    else
+    {
+        if (key < root->data)
+        {
+            height++;
+            searchNode(root->left, key);
+        }
+
+        else if (key > root->data)
+        {
+            height++;
+            searchNode(root->right, key);
+        }
+    }
+}
 void main()
 {
-    printf("\n");
-    char ch, ch1;
-    int s;
-    node *root = NULL, *tmp;
-    do
-    {
-        tmp = create();
-        if (root == NULL)
-        {
-            root = tmp;
-        }
-        else
-        {
-            insert(root, tmp);
-        }
-        printf("\nPreorder : ");
-        preorder(root);
-        printf("\nPostorder : ");
-        postorder(root);
-        printf("\nInorder : ");
-        inorder(root);
-        printf("\n\n");
-        printf("Want to enter more data? : ");
-        getchar();
-        scanf("%c", &ch);
-    } while (ch == 'Y' || ch == 'y');
+    int flag, data, s, del, search;
+    char ch;
+    struct node *root = NULL;
     line();
-    printf("\n");
-    printf("There you go...\nFinal result : \n");
-    printf("\nPreorder : ");
-    preorder(root);
-    printf("\nPostorder : ");
-    postorder(root);
-    printf("\nInorder : ");
-    inorder(root);
-    printf("\n\n");
+    while (1)
+    {
+        printf("\n1.Insert\n2.Delete\n3.Find minimum value\n4.Find maximum value\n5.Search\n6.Exit\n\n");
+        printf("Which operation you want to perform? : ");
+        scanf("%d", &s);
+        switch (s)
+        {
+        case 1:
+            do
+            {
+                line();
+                printf("Enter data : ");
+                scanf("%d", &data);
+                line();
+                root = insertNode(root, data);
+                printf("\nPreOrder : ");
+                preorder(root);
+                printf("\n");
+                printf("\nPostOrder : ");
+                postorder(root);
+                printf("\n");
+                printf("\nInOrder : ");
+                inorder(root);
+                printf("\n");
+                line();
+                printf("Want to insert more data? : ");
+                getchar();
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'Y');
+            break;
+
+        case 2:
+            do
+            {
+                line();
+                printf("Enter the data you want to delete : ");
+                scanf("%d", &del);
+                deleteNode(root, del);
+                printf("\nPreOrder : ");
+                preorder(root);
+                printf("\n");
+                printf("\nPostOrder : ");
+                postorder(root);
+                printf("\n");
+                printf("\nInOrder : ");
+                inorder(root);
+                printf("\n");
+                line();
+                printf("Want to delete more data? : ");
+                getchar();
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'Y');
+            break;
+        case 3:
+            line();
+            printf("Minimum value in this BST = %d\n", minValue_print(root));
+            break;
+        case 4:
+            line();
+            printf("Maximum value in this BST = %d\n", maxValue_print(root));
+            break;
+        case 5:
+            do
+            {
+                line();
+                printf("Search for the data : ");
+                scanf("%d", &search);
+                flag = searchNode(root, search);
+                if (flag == 1)
+                {
+                    printf("Value found at height %d", height);
+                }
+                else
+                {
+                    printf("Value not found");
+                }
+                printf("\n");
+                line();
+                printf("Want to search again? : ");
+                getchar();
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'Y');
+            break;
+        case 6:
+            line();
+            printf("\n");
+            printf("Thanks for using! Have a great day :)");
+            printf("\n");
+            line();
+            exit(0);
+            break;
+        default:
+            line();
+            printf("Inavlid entry!");
+            printf("\n");
+            break;
+        }
+        line();
+    }
 }
